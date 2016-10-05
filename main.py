6b9858b0c0cee778aa8b337a65274a8f7b9de3be
yi li <yli@yis-MacBook-Pro.local>
2016-10-05 01:41:18 -0500

    a

--- main.py
+++ main.py
@@ -45,44 +45,39 @@ if __name__ == '__main__':
     idx = 0
     while True:
         mark = False
-        print idx
+        for idx in range(dim):
+            print idx
+            pool = multiprocessing.Pool(processes = CoreNumber,
+                                    initializer = start_process)
+            inputList = []
 
-        pool = multiprocessing.Pool(processes = CoreNumber,
-                                initializer = start_process)
-        inputList = []
+            for i in range(testRange[0], testRange[1], stepLength):
+                inputList.append((i, paraList, idx))
 
-        for i in range(testRange[0], testRange[1], stepLength):
-            inputList.append((i, paraList, idx))
+            result = pool.map(mytestWarp,inputList)
+            #result = map(mytestWarp, inputList)
+            pool.close()
+            pool.join()
 
-        result = pool.map(mytestWarp,inputList)
-        #result = map(mytestWarp, inputList)
-        pool.close()
-        pool.join()
+            f = open("intersection" + str(idx) + ".txt", "a")
+            for i in result:
+                print i[0], i[1]
+                f.write(str(i[0]) +str(i[1])  + '\n')
 
-        f = open("intersection" + str(idx) + ".txt", "a")
-        for i in result:
-            print i[0], i[1]
-            f.write(str(i[0]) +str(i[1])  + '\n')
+            maxSpeed, maxThreshold = max(result, key = lambda x: x[0])
+            f.write("final:"+ str(maxSpeed) + ',' + str(maxThreshold) + ',' + str(paraList))
 
+            #minDuration, minWeThreshold, minNsThreshold = min(result, key = lambda x: x[0])
+            #f.write("final:"+ str(minDuration) + ',' + str(minWeThreshold) + ',' + str(minNsThreshold))
+            if paraList[idx] != maxThreshold:
+                mark = True
 
-
-        maxSpeed, maxThreshold = max(result, key = lambda x: x[0])
-        f.write("final:"+ str(maxSpeed) + ',' + str(maxThreshold) + ',' + str(paraList))
-
-        #minDuration, minWeThreshold, minNsThreshold = min(result, key = lambda x: x[0])
-        #f.write("final:"+ str(minDuration) + ',' + str(minWeThreshold) + ',' + str(minNsThreshold))
-        if paraList[idx] != maxThreshold:
-            mark = True
-
-        paraList[idx] = maxThreshold
-        f.flush()
-        if idx + 1 == dim and mark == False:
-                break
-        idx = (idx + 1) % dim
-        count += dim
-        time.sleep(10)
-
-        print "sleeping at loot--------"
+            paraList[idx] = maxThreshold
+            f.flush()
+            time.sleep(10)
+            print "sleeping at loot--------"
+        if False == mark:
+            break
 
     print paraList
     print count
